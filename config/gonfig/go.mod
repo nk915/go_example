c0307887ce40d684b79315ab7a0a37abda97d9f4
nk915 <nk915@hanssak.co.kr>
2021-12-06 13:43:30 +0900

    add: config->gonfig module example

---
+++ config/gonfig/go.mod
@@ -0,0 +1,9 @@
+module test-apps.com/config
+
+go 1.16
+
+require (
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/tkanos/gonfig v0.0.0-20210106201359-53e13348de2f
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ config/gonfig/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/tkanos/gonfig"
+)
+
+type Logging struct {
+	LogLevel string
+	Test     string
+}
+
+type Config struct {
+	Port    int `env:"APP_PORT"`
+	Logging map[string]interface{}
+}
+
+func main() {
+
+	config := Config{}
+	err := gonfig.GetConf(getFileName(), &config)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(500)
+	}
+
+	fmt.Println("Port: ", config.Port)
+	fmt.Println("Logging: ", config.Logging)
+	fmt.Println("Logging->LogLevel: ", config.Logging["LogLevel"])
+	//fmt.Println(config.Logging["Product"])
+	//fmt.Println(config.Logging["NullPoint"])
+
+}
+
+func getFileName() string {
+	env := os.Getenv("ENV")
+
+	if len(env) == 0 {
+		env = "dev"
+	}
+
+	filename := []string{"config/", "config.", env, ".json"}
+	_, dirname, _, _ := runtime.Caller(0)
+	filepath := path.Join(filepath.Dir(dirname), strings.Join(filename, ""))
+
+	fmt.Println("file_path: ", filepath)
+	return filepath
+}
