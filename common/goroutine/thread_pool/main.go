9b2ed468b1643e6623a177e74a7c1ffbcb29bd2b
devnk915 <devnk915@gmail.com>
2024-05-14 17:22:28 +0900

    add: goruntine

--- common/goroutine/thread_pool/main.go
+++ common/goroutine/thread_pool/main.go
@@ -9,46 +9,28 @@ import (
 )
 
 type Worker interface {
-	Work()
+	Work(string)
 	Name() string
 }
 
 // A, B, C 타입 정의
-type A struct {
-	ID string
-}
+type A struct{}
 
-func (a A) Work()         { fmt.Println("A 작업 수행") }
-func (a A) Name() string  { return "A" }
-func (a A) GetID() string { return a.ID }
-func (a A) SetID() {
-	uid := uuid.New()
-	a.ID = uid.String()
-}
+func NewA() Worker          { return &A{} }
+func (a *A) Work(id string) { fmt.Printf("A 작업 수행: %s\n", id) }
+func (a *A) Name() string   { return "A" }
 
-type B struct {
-	ID string
-}
+type B struct{}
 
-func (b B) Work()         { fmt.Println("B 작업 수행") }
-func (b B) Name() string  { return "B" }
-func (b B) GetID() string { return b.ID }
-func (b B) SetID() {
-	uid := uuid.New()
-	b.ID = uid.String()
-}
+func NewB() Worker          { return &B{} }
+func (b *B) Work(id string) { fmt.Printf("B 작업 수행: %s\n", id) }
+func (b *B) Name() string   { return "B" }
 
-type C struct {
-	ID string
-}
+type C struct{}
 
-func (c C) Work()         { fmt.Println("C 작업 수행") }
-func (c C) Name() string  { return "C" }
-func (c C) GetID() string { return c.ID }
-func (c C) SetID() {
-	uid := uuid.New()
-	c.ID = uid.String()
-}
+func NewC() Worker         { return &C{} }
+func (c C) Work(id string) { fmt.Printf("C 작업 수행: %s\n", id) }
+func (c C) Name() string   { return "C" }
 
 // ThreadPool 관리 구조체
 type ThreadPool struct {
@@ -89,6 +71,8 @@ func (tp *ThreadPool) Run(w Worker) {
 }
 
 func (tp *ThreadPool) worker(w Worker, stop chan struct{}) {
+	id := uuid.New().String()
+
 	defer func() {
 		tp.mu.Lock()
 		tp.active[w.Name()]--
@@ -102,9 +86,10 @@ func (tp *ThreadPool) worker(w Worker, stop chan struct{}) {
 	for {
 		select {
 		case <-stop:
+			fmt.Printf("%s 작업 중지: %s\n", w.Name(), id)
 			return
 		default:
-			w.Work()
+			w.Work(id)
 			time.Sleep(1 * time.Second)
 		}
 	}
@@ -124,20 +109,18 @@ func (tp *ThreadPool) Stop(name string) {
 
 func main() {
 	pool := NewThreadPool()
-	a := A{}
-	a.SetID()
-	pool.Run(a)
 
-	b := B{}
-	b.SetID()
-	pool.Run(b)
+	pool.Run(NewA())
+	pool.Run(NewB())
 	time.Sleep(3 * time.Second) // A와 B가 작업을 수행하는 동안 기다림
 
 	pool.Stop("A")
-	pool.Run(b) // B는 이미 실행 중이므로 추가 실행되지 않음
-	c := C{}
-	c.SetID()
-	pool.Run(c)
 
-	time.Sleep(5 * time.Second) // C가 작업을 수행하는 동안 기다림
+	time.Sleep(1 * time.Second)
+
+	pool.Run(NewB()) // B는 이미 실행 중이므로 추가 실행되지 않음
+
+	time.Sleep(5 * time.Second)
+
+	//	time.Sleep(5 * time.Second) // C가 작업을 수행하는 동안 기다림
 }
