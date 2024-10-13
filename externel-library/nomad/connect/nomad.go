19c10ff582b6874e25b400e6d08ba89343231248
devnk915 <devnk915@gmail.com>
2024-10-13 14:58:11 +0900

    Add: nomad API

--- externel-library/nomad/connect/nomad.go
+++ externel-library/nomad/connect/nomad.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"net/http"
 
 	"github.com/hashicorp/nomad/api"
 )
@@ -9,19 +11,62 @@ import (
 func main() {
 	// Nomad API 클라이언트 설정
 	config := api.DefaultConfig()
-	config.Address = "http://192.168.20.120:4646"
+	config.Address = "https://192.168.20.120:4646"
+	//config.Address = "https://192.168.20.202:4646"
+
+	// Set up a custom HTTP client with InsecureSkipVerify
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	transport := &http.Transport{
+		TLSClientConfig: tlsConfig,
+	}
+
+	config.HttpClient = &http.Client{
+		Transport: transport,
+	}
+
 	client, err := api.NewClient(config)
 	if err != nil {
 		fmt.Println("Nomad 클라이언트 생성 실패:", err)
 		return
 	}
 
+	status(client)
+	jobs(client)
+}
+
+func status(client *api.Client) {
 	// 리더 정보 조회
-	status, err := client.Status().Leader()
+	status := client.Status()
+
+	leader, err := status.Leader()
 	if err != nil {
 		fmt.Println("리더 정보 조회 실패:", err)
 		return
 	}
+	fmt.Println("현재 리더 주소:", leader)
 
-	fmt.Println("현재 리더 주소:", status)
+	peer, err := status.Peers()
+	if err != nil {
+		fmt.Println("Peer 정보 조회 실패:", err)
+		return
+	}
+	fmt.Println("현재 Peer 주소:", peer)
+
+}
+
+func jobs(client *api.Client) {
+	// Fetch the list of jobs
+	jobs, _, err := client.Jobs().List(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Nomad Jobs:")
+	for _, job := range jobs {
+		fmt.Printf("Job: %s, Status: %s\n", job.Name, job.Status)
+		//fmt.Printf("Job: %+v\n", job)
+	}
 }
